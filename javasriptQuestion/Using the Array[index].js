558f109215745155bb40fb795694b18e765b66ab
unknown <rohit995379@gmail.com>
2025-05-12 01:31:40 +0530

    commit

--- javasriptQuestion/Using the Array[index].js
+++ javasriptQuestion/Using the Array[index].js
@@ -16,18 +16,18 @@ let jsonArrayDatas = [
         "ComapnyName": "GeeksforGeeks",
         "Location": "Noida"
     },
-    
+
     {
         "ComapnyName": "GeeksforGeeks",
         "Location": "Noida"
     },
-    
-    
+
+
     {
         "topics":
             [
                 "DSA self paced course",
-                "Machine learning",
+                "Machinelearning",
                 "Web development",
                 "Block Chain",
                 "Artificial Intelligence"
@@ -38,5 +38,14 @@ let jsonArrayDatas = [
 // console.log(jsonArrayDatas[0]["Location"]);
 
 for (let element in jsonArrayDatas) {
-    console.log(element);
+    // console.log(element);
+    if (jsonArrayDatas[element].topics) {
+        // console.log(jsonArrayDatas[element]["topics"].Machinelearning);
+    }
 }
+
+jsonArrayDatas.forEach((element) => {
+    if (element.topics) {
+        console.log(element.topics[0], element.topics[2]);
+    }
+})
