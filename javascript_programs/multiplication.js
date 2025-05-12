64e8aa12d31b8b8cf02d5152bbc7766a6471e14b
unknown <rohit995379@gmail.com>
2025-05-13 00:19:03 +0530

    commit

--- javascript_programs/multiplication.js
+++ javascript_programs/multiplication.js
@@ -1,22 +1,34 @@
-let a = 10 ;
-let b = 20 ;
+let a = 10;
+let b = 20;
 
 
 let c = a * b;
 console.log(c)/// write a function multiplication
-function multiplication(){
+function multiplication() {
     return a * b
-}/console.log(multiplication())///Using Arrow function
-let twoMultiple =() =>{
+} 
+console.log(multiplication())///Using Arrow function
+let twoMultiple = () => {
     return a * b
 }
+
+
+
 console.log(twoMultiple())///Using Multiplication assignment operato/// Multiplicationassignmentoperator
- console.log(a *= b);
+console.log(a *= b);
 // Using for loop
- let arr = [a ,b]
-//  console.log(arr)
- for(let i = 0; i <= arr.length - 1  ; i++){
+let arr = [a, b]
+for (let i = 0; i <= arr.length - 1; i++) {
     console.log(arr[0] * arr[1])
     break;
-    //  console.log(a * b);
- }
+}
+
+function multiplication(a, b) {
+    let result;
+    for (let i = 0; i <= b; i++) {
+        result = a * b
+        break;
+    }
+    return result;
+}
+console.log(multiplication(18, 20))///Using for loop
