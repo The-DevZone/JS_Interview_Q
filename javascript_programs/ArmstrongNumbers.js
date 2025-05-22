1287b269c0166e7783597fbe47201d9d4884fb65
unknown <rohit995379@gmail.com>
2025-05-22 23:59:36 +0530

    commit

--- javascript_programs/ArmstrongNumbers.js
+++ javascript_programs/ArmstrongNumbers.js
@@ -44,3 +44,14 @@ let heightLength = 5;
 let tringle = areaTringle(baseLength , heightLength);
 console.log("this is a area of tringle" + tringle);
  
+function findLargest(n1, n2, n3){
+    if(n1 >= n2 && n1 >= n3){
+        return n1;
+    }else if(n2 > n1 &&  n2 >= n3){
+        return n2;
+    }else{
+        return n3;
+    }
+}
+
+console.log(findLargest(10, 20, 50))
